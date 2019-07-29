0c84812e25aaa8699bedfc16bc33c9f3a3f23681
Mitchell <mmoscovics@yahoo.com>
2019-07-29 19:05:06 +0000

    update switch program adjusted variable and data types names

--- switch.c
+++ switch.c
@@ -5,19 +5,19 @@
  *@spec: passed in specifier from get_spec
  */
 
-void *switch_help(char *spec, ...)
+int switch_help(char spec, char *list)
 {
 	switch (spec)
 	{
 	case 'c':
-		_putchar(va_arg(char));
+		_putchar(list);
 	case 'i':
 	case 'd':
-		_puts(va_arg(int));
+		_puts(list);
 	case 's':
-		_puts(va_arg(char *));
+		_puts(list);
 	case '%':
-		_putchar(va_arg(char));
+		_putchar(list);
 	/* case 'b': */
 	/* 	printbinary(); */
 	/* case 'u': */
